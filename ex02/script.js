83931cb681db8d62d265150d881254a72d7c1e69
marvinseran <marvinseran@gmail.com>
2024-01-11 16:05:49 +0100

    push

--- ex02/script.js
+++ ex02/script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', function () {
 
     const fetchButton = document.getElementById('FetchButton');
@@ -15,16 +14,17 @@ document.addEventListener('DOMContentLoaded', function () {
 
     fetchButton.addEventListener('click', function () {
 
-        const name = nameInput.value.trim();
+        const word = nameInput.value.trim();
         const country = countrySelect.value;
+        
 
-        if (name !== '') {
+        if (word !== '') {
             const requestDiv = document.createElement('div');
 
-            fetch(`https://api.agify.io?name=${name}&country_id=${country}`)
+            fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
                 .then(response => response.json())
                 .then(data => {
-                    requestDiv.innerHTML = `<strong>${name}</strong> est ajouté depuis ce pays: ${country}`;
+                    requestDiv.innerHTML = `<strong>${word}</strong> est ajouté depuis ce pays: ${country} </br> Définition : ${data[0].meanings[0].definitions[0].definition}`;
                     resultContainer.appendChild(requestDiv);
 
                     previousResults.push(requestDiv.innerHTML);
